bd258d16a561793ac50dad556b9919d5a6a043c2
Mona04 <45962492+Mona04@users.noreply.github.com>
2019-06-10 21:07:17 +0900

    Update 100.cpp

--- 100/100.cpp
+++ 100/100.cpp
@@ -5,7 +5,7 @@
  * - The presence of overhead due to locks.
  *
  */
-/*
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
@@ -122,4 +122,3 @@ long timediff(clock_t t1, clock_t t2) {
     elapsed = ((double)t2 - t1) / CLOCKS_PER_SEC * 1000;
     return elapsed;
 }
-*/
